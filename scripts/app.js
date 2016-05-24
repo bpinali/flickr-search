9e2f91862e72c7d6acaffa887c5e6691273ee071
Ben Pinali <ben@benpinali.com>
2016-05-24 18:56:53 -0500

    Dynamical inject image URLs into inline CSS styles using ng-style

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,27 @@
-angular.module('flickrSearch', [])
-    .controller('flickrController', function($scope, $http) {
-        $scope.results = [];
-        $scope.tagSearch = function(text) {
-            //console.log("I'm working here!");
-            //console.log($scope.searchTag)
-            var url = "https://api.flickr.com/services/rest";
-            var request = {
-                method: 'flickr.photos.search',
-                api_key: "3abfc0179898d3ad907be5001f4b3933",
-                tags: $scope.searchTag,
-                format: 'json',
-                nojsoncallback: 1
-            };
-            console.log(request);
-            $http({
-                method: 'GET',
-                url: url,
-                params: request
-            }).success(function(data) {
-                $scope.results = data;
-                //console.log(data);
-            }).error(function(error) {
-                //console.error(error);
-            });
+var app = angular.module('flickrSearch', []);
+app.controller('flickrController', function($scope, $http) {
+    $scope.results = [];
+    $scope.tagSearch = function(text) {
+        //console.log("I'm working here!");
+        //console.log($scope.searchTag)
+        var url = "https://api.flickr.com/services/rest";
+        var request = {
+            method: 'flickr.photos.search',
+            api_key: "3abfc0179898d3ad907be5001f4b3933",
+            tags: $scope.searchTag,
+            format: 'json',
+            nojsoncallback: 1
         };
-    });
+        console.log(request);
+        $http({
+            method: 'GET',
+            url: url,
+            params: request
+        }).success(function(data) {
+            $scope.results = data;
+            //console.log(data);
+        }).error(function(error) {
+            //console.error(error);
+        });
+    };
+});
